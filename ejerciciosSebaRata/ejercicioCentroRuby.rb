4eb3a9bcd327069d1de733857d65eab7789356ba
Tomas Iparraguirre <tomiparrag@gmail.com>
2025-05-22 01:25:52 +0000

    Ejercicios de Js hechos en ruby

--- ejerciciosSebaRata/ejercicioCentroRuby.rb
+++ ejerciciosSebaRata/ejercicioCentroRuby.rb
@@ -1,21 +1,22 @@
-def ejercicioCentro(c1,c2)
-	return "Coordenadas Invalidas"	 unless sonCoordenadasValidas(c1,c2)
-		centro = calcularCentro(c1,c2)
-		return "centro #{centro}"
+def ejercicio_centro(c1, c2)
+  return "Coordenadas Invalidas" unless son_coordenadas_validas(c1, c2)
+  centro = calcular_centro(c1, c2)
+  "centro #{centro}"
 end
 
-def sonCoordenadasValidas(c1, c2)
-  coordsJuntas = c1 + c2
-  return false unless coordsJuntas.length == 4
-  return false unless coordsJuntas.all? { |pos| pos.is_a?(Numeric) && pos.between?(0, 100) }
+def son_coordenadas_validas(c1, c2)
+  coords_juntas = c1 + c2
+  return false unless coords_juntas.length == 4
+  return false unless coords_juntas.all? { |pos| pos.is_a?(Numeric) && pos.between?(0, 100) }
   true
 end
 
-def calcularCentro(c1,c2)
-	centro = [((c1[0]+ c2[0]) / 2 ).ceil, ((c1[1] + c1[1]) / 2).ceil]
-	centro
+def calcular_centro(c1, c2)
+  centro = [((c1[0] + c2[0]) / 2).ceil, ((c1[1] + c2[1]) / 2).ceil]
+  centro
 end
 
-	c1 = [42,4]
-	c2 = [2,3]
-ejercicioCentro(c1,c2)
+COORDENADA_1 = [42, 4]
+COORDENADA_2 = [2, 3]
+
+ejercicio_centro(COORDENADA_1, COORDENADA_2)
